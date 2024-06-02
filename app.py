91b0b66711953b353cde24a802e868189737596b
Bird Johnson <jeepinbird@gmail.com>
2024-06-02 15:51:24 -0500

    Working version

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import ollama
+import search
 import streamlit as st
 
 st.title("Second Brain")
@@ -28,11 +29,16 @@ with st.sidebar:
         if "initial_prompt" in st.session_state:
             del st.session_state["initial_prompt"]
 
+# Get initial context from the database
+@st.cache_data(show_spinner='Fetching context from PostgreSQL')
+def get_context(prompt):
+    return search.get_context(prompt)
+
 # Generate streaming response
-def model_res_generator():
+def model_resp_generator():
     stream = ollama.chat(
         model=st.session_state["model"],
-        messages=st.session_state["messages"],
+        messages=st.session_state["initial_context"] + st.session_state["messages"],
         stream=True,
     )
     for chunk in stream:
@@ -46,17 +52,18 @@ for message in st.session_state["messages"]:
 if prompt := st.chat_input("Enter prompt here"):
     # add latest message to history in format {role, content}
     st.session_state["messages"].append({"role": "user", "content": prompt})
-
+    
     # initialize database connection and check database for answers
     if "initial_prompt" not in st.session_state:
-        st.write(f"initial_prompt: {prompt}")
         st.session_state["initial_prompt"] = prompt
-        #conn = st.connection('second_brain', type='sql')
+        st.session_state["initial_context"] = [{"role": "user", "content": f"Using the following text from my personal journal as a resource\n```\n{get_context(prompt)}\n```\n\nAnswer the question: {prompt}"}]
+        print(f"\n\nprompt: {st.session_state["initial_prompt"]}")
+        print(f"\ncontext: {st.session_state["initial_context"]}")
 
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        message = st.write_stream(model_res_generator())
+        message = st.write_stream(model_resp_generator())
         st.session_state["messages"].append({"role": "assistant", "content": message})
 
--- search.py
+++ search.py
@@ -1,12 +1,10 @@
-import sys
 import toml
 import ollama
 import psycopg
 import numpy as np
-import pandas as pd
 from pgvector.psycopg import register_vector
 
-# Read the password from the TOML file
+# Read the password from the TOML secrets file
 config = toml.load('.streamlit/secrets.toml')
 db_config = config['connections']['second_brain']
 
@@ -30,33 +28,40 @@ def get_context(query_string):
             
             # Attempt to find relevant context using these various queries
             try:
-                # Execute queries and fetch results into pandas DataFrames
-                results1 = pd.read_sql("SELECT date, blurb, content FROM journal.search_events(%s)", conn, params=(query_string,))
-                results2 = pd.read_sql("SELECT date, blurb, '' AS content FROM journal.search_entries(%s) LIMIT 20", conn, params=(query_string,))
-                results3 = pd.read_sql("SELECT a.date, a.blurb, b.content FROM journal.entries a JOIN journal.events b ON b.entry_id = a.id ORDER BY b.embedding <-> %s LIMIT 10", conn, params=(embed_qry,))
-                
-                # Combine all results into a single DataFrame
-                results = pd.concat([results1, results2, results3])
+                results = conn.execute("SELECT date, blurb, '' AS content FROM journal.search_entries(%s)", (query_string,)).fetchall()
+                results.extend(conn.execute("SELECT date, blurb, content FROM journal.search_events(%s) LIMIT 20", (query_string,)).fetchall())
+                results.extend(conn.execute("SELECT a.date, a.blurb, b.content FROM journal.entries a JOIN journal.events b ON b.entry_id = a.id ORDER BY b.embedding <-> %s LIMIT 20", (embed_qry,)).fetchall())
             except psycopg.Error as e:
                 print("Error occurred: {}",format(e))
-            else:
-                return results
     except psycopg.OperationalError as e:
         print("Unable to connect to the database: {}".format(e))
+    
+    context = "\n\n".join([f"{date}: {blurb} - {content}" for date, blurb, content in results])
+    return context
 
-query = sys.argv[1]
+if __name__ == "__main__":
+    import sys
+    
+    query = sys.argv[1]
 
-docs = get_context(query)
+    streamed_response = True
+    if run_type := sys.argv[2]:
+        if run_type == 'no_stream':
+            streamed_response = False
 
-# Extract the relevant content from the DataFrame
-context = "\n\n".join([f"{row['date']}: {row['blurb']} - {row['content']}" for _, row in docs.iterrows()])
+    context = get_context(query)
 
-modelquery = f"Using the following text from my personal journal as a resource\n```\n{context}\n```\n\nAnswer the question: {query}"
+    modelquery = f"Using the following text from my personal journal as a resource\n```\n{context}\n```\n\nAnswer the question: {query}"
 
-stream = ollama.generate(model='llama3', prompt=modelquery, stream=True)
+    response = ollama.generate(model='llama3', prompt=modelquery, stream=streamed_response)
 
-for chunk in stream:
-  if chunk["response"]:
-    print(chunk['response'], end='', flush=True)
+    if streamed_response:
+        for chunk in response:
+            if chunk["response"]:
+                print(chunk['response'], end='', flush=True)
+    else:
+        print(f"Evaluated {response['prompt_eval_count']} tokens and generated the full response in {response['total_duration'] / 1000000000} seconds.")
+        print(response["response"])
 
-print()
+    # End with a new line
+    print()
