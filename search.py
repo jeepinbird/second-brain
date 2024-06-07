15b73935cea05bf242f8ce965daecd409375591c
Bird Johnson <jeepinbird@gmail.com>
2024-06-07 08:25:11 -0500

    Cleaned up results

--- search.py
+++ search.py
@@ -28,15 +28,15 @@ def get_context(query_string):
             
             # Attempt to find relevant context using these various queries
             try:
-                results = conn.execute("SELECT date, blurb, '' AS content FROM journal.search_entries(%s)", (query_string,)).fetchall()
-                results.extend(conn.execute("SELECT date, blurb, content FROM journal.search_events(%s) LIMIT 20", (query_string,)).fetchall())
-                results.extend(conn.execute("SELECT a.date, a.blurb, b.content FROM journal.entries a JOIN journal.events b ON b.entry_id = a.id ORDER BY b.embedding <-> %s LIMIT 20", (embed_qry,)).fetchall())
+                results = conn.execute("SELECT date, CASE WHEN blurb IS NULL THEN '' ELSE blurb || ' - ' END AS blurb, '' AS content FROM journal.search_entries(%s)", (query_string,)).fetchall()
+                results.extend(conn.execute("SELECT date, CASE WHEN blurb IS NULL THEN '' ELSE blurb || ' - ' END AS blurb, content FROM journal.search_events(%s) LIMIT 20", (query_string,)).fetchall())
+                results.extend(conn.execute("SELECT a.date, CASE WHEN a.blurb IS NULL THEN '' ELSE a.blurb || ' - ' END AS blurb, b.content FROM journal.entries a JOIN journal.events b ON b.entry_id = a.id ORDER BY b.embedding <-> %s LIMIT 20", (embed_qry,)).fetchall())
             except psycopg.Error as e:
                 print("Error occurred: {}",format(e))
     except psycopg.OperationalError as e:
         print("Unable to connect to the database: {}".format(e))
     
-    context = "\n\n".join([f"{date}: {blurb} - {content}" for date, blurb, content in results])
+    context = "\n\n".join([f"{date}: {blurb}{content}" for date, blurb, content in results])
     return context
 
 if __name__ == "__main__":
@@ -47,6 +47,7 @@ if __name__ == "__main__":
 
     # Fetch the related context from the database
     context = get_context(query)
+    print(context)
 
     # Build a prompt
     modelquery = f"Using the following text from my personal journal as a resource\n```\n{context}\n```\n\nAnswer the question: {query}"
@@ -54,7 +55,7 @@ if __name__ == "__main__":
     # Generate a response from the model
     response = ollama.generate(model='llama3', prompt=modelquery, stream=True)
 
-    # Loop through the streaming response and print out the results
+    # # Loop through the streaming response and print out the results
     for chunk in response:
         if chunk["response"]:
             print(chunk['response'], end='', flush=True)
