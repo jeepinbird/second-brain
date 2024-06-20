b82f91f3b8363f1ee07b0cbb6a5db9dafb99664c
Bird Johnson <jeepinbird@gmail.com>
2024-06-20 07:42:20 -0500

    Adding PostgreSQL DDL scripts

--- search.py
+++ search.py
@@ -9,28 +9,26 @@ config = toml.load('.streamlit/secrets.toml')
 db_config = config['connections']['second_brain']
 
 def connect_db():
-    return psycopg.connect(
-        host=db_config['host'],
-        dbname=db_config['database'],
-        user=db_config['username'],
-        password=db_config['password'],
-        autocommit=False
-    )
+    connection = psycopg.connect(
+                host=db_config['host'],
+                dbname=db_config['database'],
+                user=db_config['username'],
+                password=db_config['password'],
+                autocommit=False
+            )
+    
+    # Set up pgvector on the connection
+    register_vector(connection)
+
+    return connection
 
 def get_context(query_string):
-    # Generate embedded query
-    embed_qry = np.array(ollama.embeddings(model='nomic-embed-text', prompt=query_string)['embedding'])
-        
     try:
         with connect_db() as conn:
-            # Set up pgvector on the connection
-            register_vector(conn)
-            
             # Attempt to find relevant context using these various queries
             try:
-                results = conn.execute("SELECT date, blurb, '' AS content FROM journal.search_entries(%s)", (query_string,)).fetchall()
-                results.extend(conn.execute("SELECT date, blurb, content FROM journal.search_events(%s) LIMIT 20", (query_string,)).fetchall())
-                results.extend(conn.execute("SELECT a.date, a.blurb, b.content FROM journal.entries a JOIN journal.events b ON b.entry_id = a.id ORDER BY b.embedding <-> %s LIMIT 20", (embed_qry,)).fetchall())
+                results = conn.execute("SELECT date, CASE WHEN blurb IS NULL THEN '' ELSE blurb || ' - ' END AS blurb, '' AS content FROM journal.search_entries(%s)", (query_string,)).fetchall()
+                results.extend(conn.execute("SELECT date, CASE WHEN blurb IS NULL THEN '' ELSE blurb || ' - ' END AS blurb, content FROM journal.search_events(%s) LIMIT 20", (query_string,)).fetchall())
             except psycopg.Error as e:
                 print("Error occurred: {}",format(e))
     except psycopg.OperationalError as e:
